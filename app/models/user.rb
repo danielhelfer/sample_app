193742273077cdfd09c79c4bf8c486db6f91672f
Daniel Helfer <daniel.helfer@gmx.ch>
2012-05-21 19:00:33 +0200

    Changes due to examples

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   
-  before_save do |user|
-    user.email = email.downcase
-  end
+  before_save { |user| user.email.downcase! }
   
   validates :name, presence: true, length: { maximum: 50 }
   
